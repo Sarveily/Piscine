4e558eb52cf1a544b748ae2443da71879383ebb7
Gabriel <sarveily.pro@gmail.com>
2026-09-04 13:18:44 +0200

    modif header

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,37 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarveily <sarveily@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/04 07:00:00 by sarveily          ###   ########.fr       */
+/*   Updated: 2026/09/04 13:18:18 by sarve            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void 	ft_print_comb2 (void)
+void	ft_print_comb2(void)
 {
-	int a = 0;
-	int b;
+	int	a;
+	int	b;
 
+	a = 0;
 	while (a <= 98)
 	{
 		b = a + 1;
-		while (b < 99)
+		while (b <= 99)
 		{
-
 			write(1, &(char){a / 10 + '0'}, 1);
 			write(1, &(char){a % 10 + '0'}, 1);
-
-			write (1," ",1);
-
-			write (1, &(char){b / 10 + '0'}, 1);
-			write (1, &(char){b % 10 + '0'}, 1);
-
+			write(1, " ", 1);
+			write(1, &(char){b / 10 + '0'}, 1);
+			write(1, &(char){b % 10 + '0'}, 1);
 			if (a != 98 || b != 99)
-				write (1, ", ", 2);
-
+				write(1, ", ", 2);
 			b++;
-
 		}
 		a++;
 	}
-
 }
 
-int main (void){
-
-ft_print_comb2();
-return (0);
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
 }
